c9fc49e8731a253a9298d98152bcfca4c43219ed
Albert Comes i Bonet <treblast5@gmail.com>
2018-05-09 17:51:24 +0200

    Main menu + .gitignore

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+void main() {
+
+    int opcio = 0;
+
+    while (opcio != 4) {
+
+        mostrarMenu();
+        printf("\nOpcion: ");
+        scanf("%d", &opcio);
+        printf("\n");
+
+        switch (opcio) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                printf("\nEsta no es una opcion valida!\n");
+                break;
+        }
+    }
+}
+
+void mostrarMenu() {
+    printf("\nBlackjack\n");
+    printf("\n 1. Generar pila de cartas");
+    printf("\n 2. Jugar");
+    printf("\n 3. Mostrar estadisticas");
+    printf("\n 4. Salir\n");
 }
