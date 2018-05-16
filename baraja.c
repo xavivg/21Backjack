b27b6f18891c5fdeb851dbbf704a07ec8f9b106d
xavivg <xavivilardellgras@gmail.com>
2018-05-16 15:37:27 +0200

    MakeList modificado para poder compilar

--- baraja.c
+++ baraja.c
@@ -1,13 +1,30 @@
+#include <stdlib.h>
 #include "baraja.h"
 
 Baraja BARAJA_crea(int numero){
     Baraja b;
-    b.cuantos = 0;
+    char cartas[13] = {'A','2','3','4','5','6','7','8','9','0','J','Q','K'};
+    int r = 0;
+    int total = numero * 13 * 4; //guarda en una variable el num de cartas que entrarán en la baraja
+    b.cartas[total];//array de cartas totales segun la baraja
+    b.nCartas = 0;
+        //fer un bucle segons el numero de barajas que volen crear la pila
+        for(int j=0;j<total;j++){
+
+             r =  rand() % 13;
+             BARAJA_push(&b, cartas[r]);
+
+        }
+        if(b.nCartas == total){
+            printf("\n Pila generada correctamente de %d cartas \n", total);
+        } else{
+            printf("\n Hubo un error al crear la pila \n");
+        }
     return b;
 }
-void BARAJA_push(Baraja *b, int elemento){//agregacion de carta a la pila baraja
-
-        (*b).elementos[(*b)cuantos] = elemento;
-        (*b).cuantos++;
+void BARAJA_push(Baraja *b, char carta){//agregacion de carta a la pila baraja
+        (*b).cartas[(*b).nCartas] = carta;
+         printf("%c ", (*b).cartas[b->nCartas]);// si no es fa el printf aqui, surten caracters raros
+        (*b).nCartas++;
 
 }
